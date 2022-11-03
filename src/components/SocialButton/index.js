b54688b1cfabe3b6628d7a4b8ad1bc2db79fd3e8
Rekhasj <rekhasj21@gmail.com>
2022-11-03 15:41:56 +0000

    concept

---
+++ src/components/SocialButton/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const SocialButton = props => {
+  console.log(props)
+
+  return (
+    <>
+      <button className="social-button">{props.children}</button>
+
+      {props.children}
+    </>
+  )
+}
+
+export default SocialButton
